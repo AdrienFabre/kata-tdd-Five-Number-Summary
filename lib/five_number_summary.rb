054d8096f2453c86de6082da9fa3cce8347744cf
AdrienFabre <adrien.fabre.1@gmail.com>
2019-03-10 19:00:21 +0000

    The Median number is the middle number

--- lib/five_number_summary.rb
+++ lib/five_number_summary.rb
@@ -1,3 +1,14 @@
 def five_num(array)
-  array.sort 
+  array.sort!
+  new_array = []
+  new_array << array.first
+  new_array << "First Quartile"
+  if array.length.even?
+    new_array << (array[array.length / 2] + array[array.length / 2 - 1]) / 2.0
+  else
+    new_array << (array)[array.length / 2]
+  end
+  new_array << array.last
+  print new_array
+  new_array 
 end
--- spec/five_number_summary_spec.rb
+++ spec/five_number_summary_spec.rb
@@ -1,13 +1,21 @@
 require 'five_number_summary.rb'
 
-ARRAY = [7, 15, 36, 39, 40, 41, 42, 43, 47, 49, 6]
-
+ARRAYODD = [7, 15, 36, 39, 40, 41, 42, 43, 47, 49, 6]
+ARRAYEVEN = [1, 2, 3, 4, 5, 6]
 describe 'five_number_summary' do
   it 'the first number is the minimum value' do
-    expect(five_num(ARRAY).first).to equal 6
+    expect(five_num(ARRAYODD).first).to equal 6
   end
 
   it 'the last number in the maximun value' do 
-    expect(five_num(ARRAY).last).to equal 49
+    expect(five_num(ARRAYODD).last).to equal 49
+  end
+
+  it 'the middle number is the median for an odd array' do 
+    expect(five_num(ARRAYODD)[2]).to equal 40
+  end
+
+  it 'the middle number is the median for an even array' do 
+    expect(five_num(ARRAYEVEN)[2]).to equal 3.5
   end
 end
