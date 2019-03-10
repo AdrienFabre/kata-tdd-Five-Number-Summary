0ed4eec5ba19602abf5a51e4ad3b6cd86d849b2d
AdrienFabre <adrien.fabre.1@gmail.com>
2019-03-10 20:53:10 +0000

    The forth number is the third quartile

--- lib/five_number_summary.rb
+++ lib/five_number_summary.rb
@@ -3,16 +3,20 @@ def five_num(array)
   new_array = []
   new_array << array.first
   if array.length.even?
-    low_array = (array[0, array.length / 2 ])
+    low_array = (array[0, array.length / 2])
+
     new_array << low_array[low_array.length / 2]
     new_array << (array[array.length / 2] + array[array.length / 2 - 1]) / 2.0
+    high_array = array[array.length / 2, array.length]
+    new_array << (high_array[high_array.length / 2])
   else
-    low_array = (array[0, array.length / 2 + 1 ])
+    low_array = (array[0, array.length / 2 + 1])
+  
     new_array << (low_array[low_array.length / 2] + low_array[low_array.length / 2 - 1]) / 2.0
     new_array << array[array.length / 2]
+    high_array = array[array.length / 2, array.length]
+    new_array << (high_array[high_array.length / 2] + high_array[high_array.length / 2 - 1]) / 2.0
   end
 
   new_array << array.last
-  print new_array
-  new_array 
 end
--- spec/five_number_summary_spec.rb
+++ spec/five_number_summary_spec.rb
@@ -2,6 +2,7 @@ require 'five_number_summary.rb'
 
 ARRAYODD = [7, 15, 36, 39, 40, 41, 42, 43, 47, 49, 6]
 ARRAYEVEN = [1, 2, 3, 4, 5, 6]
+
 describe 'five_number_summary' do
   it 'the first number is the minimum value' do
     expect(five_num(ARRAYODD).first).to equal 6
@@ -25,5 +26,13 @@ describe 'five_number_summary' do
 
   it 'the second number is the first quartile for an even array' do 
     expect(five_num(ARRAYEVEN)[1]).to equal 2
+  end
+
+  it 'the forth number is the third quartile for an odd array' do 
+    expect(five_num(ARRAYODD)[3]).to equal 42.5
+  end
+
+  it 'the forth number is the third quartile for an odd array' do 
+    expect(five_num(ARRAYEVEN)[3]).to equal 5
   end
 end
